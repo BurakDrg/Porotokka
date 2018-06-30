657aa7cdcd7916f3d8b8bfd41db6a130ba10c5c4
berk-12 <berkbacalan@gmail.com>
2018-06-30 21:05:56 +0300

    Database files added.

---
+++ PorotokkaServer/db/Database.java
@@ -0,0 +1,16 @@
+import android.content.Context;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+public class Database extends SQLiteAssetHelper {
+    private static final String DATABASE_NAMES = "events";
+    private static final int DATABASE_VERSION = 3;
+    public Database(Context context) {
+        super(context, DATABASE_NAMES, null, DATABASE_VERSION);
+    }
+CREATE TABLE events.eventTAble(
+   EVENT_ID NUMERIC,
+   EVENT_NAME TEXT,
+   MESSAGE TEXT,
+   TIME NUMERIC,
+   DATE NUMERIC
+);
+}
