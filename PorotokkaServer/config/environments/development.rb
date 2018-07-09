278527584c8779f37f0473340adb9d3eea694830
BurakDrg <burakdurgunsu@hotmail.com>
2018-07-10 01:27:46 +0300

    New Server added

--- PorotokkaServer/config/environments/development.rb
+++ PorotokkaServer/config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
   # Show full error reports.
   config.consider_all_requests_local = true
 
