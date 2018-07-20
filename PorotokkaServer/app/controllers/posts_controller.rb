ae31530bfead801a333bf7224cffd75c6521133e
BurakDrg <burakdurgunsu@hotmail.com>
2018-07-20 11:53:42 +0300

    Backend Processing added

--- PorotokkaServer/app/controllers/posts_controller.rb
+++ PorotokkaServer/app/controllers/posts_controller.rb
@@ -1,13 +1,10 @@
-require 'sidekiq'
-
 class PostsController < ApplicationController
-  include Sidekiq::Worker
-
   before_action :find_post, only: [:edit, :update, :destroy]
-  #before_action :deneme3
+  after_action :notification, only: [:create]
   skip_before_action :verify_authenticity_token
   def index
     @post = Post.all
+
   end
 
   def new
@@ -15,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    require 'sidekiq'
     @post = Post.new(post_params)
 
     if @post.save
@@ -23,6 +20,14 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    #render json: {event_name:@post.event_name, event_date:@post.event_date, event_time:@post.event_time}
+  end
+
+  def notification
     eventtime = @post.event_time
     time = eventtime[0,2].split(':')[0].to_i
 
@@ -35,36 +40,47 @@ class PostsController < ApplicationController
       counter = counter + 1
     end
     day = monthdata[0].to_i
-    mounth = counter
+    month = counter
     year = monthdata[2].to_i
 
-    puts counter
-  end
-
-  def first(limit = 1)
-    if limit == 0
-      ''
-    elsif limit >= size
-      self.dup
+    createdDate = @post.created_at.to_s
+    difYear =  year - createdDate[0,4].to_i - 1
+    difMonth = month - createdDate[5,7].to_i - 1
+    difDay = day - createdDate[8,10].to_i - 1
+    difHour = createdDate[11,13].to_i + 2
+    if difHour > 24
+      difHour = time - (difHour - 24)
+      difDay = difDay - 1
     else
-      to(limit - 1)
+      difHour = time - difHour
     end
-  end
+    difMin = 60 - createdDate[14,16].to_i
+    waitTime = 0.to_f
 
-  def show
-    @post = Post.find(params[:id])
-    NotificationJob.perform_later guest
-    #render json: {event_name:@post.event_name, event_date:@post.event_date, event_time:@post.event_time}
-  end
-
-  def notification
-    fcm_client = FCM.new("AAAAcIjcbiM:APA91bH2h-Y5b20cq72sZPC9rLPvkJMGRFvPDf8ONZzhmeoJj-L7zbhtStJdKVnCeNUB3sqzdiBW6AyRnIwjhAxmOzZ9oO9e-RK1Ym3Tk9LC3oqQYx6UhiaYSlA9Z1ScYNcEfUDtMEbg_E3PnMn9Qe3uoKyua5b-oQ")
-
-     options = {priority: 'high',data: {message: 'message',icon: ''},notification: {body: "message",sound: 'default',icon: ""}}
-
-     user_device_ids=["eWz7xsbHr2Q:APA91bGvP_DaPEUpXdsd3ButywJm8BQE21CNAyPXuaM2sbDBHWRjNr8-z1mXJ0tbdfB2izgf3cOiEp9HyIqKSzKk_WCQXVsKPoaqfqY2DFuXZLj2psXPrCmcAAmAD2mFpptlXn18MKy4HfO_oo93MAGqOZ4xhHAA3Q"]
-     response =  fcm_client.send(device_ids, options)
-
+    if difYear > 0
+      waitTime = difYear.to_f + (difMonth/12).to_f + (difDay/365).to_f + difHour.to_f/(365*24).to_f + (difMin-15).to_f/(365*24*60).to_f
+      NotificationWorker.perform_in(waitTime.years,@post.event_name)
+      puts "Yıl"
+    elsif difMonth > 0
+      waitTime = difMonth.to_f + (difDay/30).to_f + difHour.to_f/(30*24).to_f + (difMin-15).to_f/(30*24*60).to_f
+      NotificationWorker.perform_in(waitTime.months,@post.event_name)
+      puts "Ay"
+    elsif difDay > 0
+      waitTime = difDay.to_f + (difHour/24).to_f + (difMin-15).to_f/(24*60).to_f
+      NotificationWorker.perform_in(waitTime.days,@post.event_name)
+      puts "Gün"
+    elsif difHour > 10
+      waitTime = difHour.to_f + (difMin-15).to_f/(60).to_f
+      NotificationWorker.perform_in(waitTime.hours,@post.event_name)
+      puts "Saat"
+    elsif difMin > 150
+      waitTime = difMin-15
+      NotificationWorker.perform_in(waitTime.minutes,@post.event_name)
+      puts "Dakika"
+    else
+      NotificationWorker.perform_async(@post.event_name)
+      puts "Hemen"
+    end
   end
 
   def edit
@@ -93,6 +109,11 @@ class PostsController < ApplicationController
   end
 
   def post_params
+    #params.permit(:event_name, :event_date, :event_time)
     params.require(:post).permit(:event_name, :event_date, :event_time)
+  end
+
+  def startWorker
+
   end
 end
--- PorotokkaServer/config/routes.rb
+++ PorotokkaServer/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   resources :posts
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
