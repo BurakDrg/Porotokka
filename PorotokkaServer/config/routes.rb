d39a0b176354aa76b2d274e41c90d9971b35bcc5
berk-12 <berkbacalan@gmail.com>
2018-08-07 01:47:03 +0300

    last version of web app.

--- PorotokkaServer/config/routes.rb
+++ PorotokkaServer/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+root :to => 'home#index'
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
 
@@ -7,5 +8,8 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get '/all', to: 'posts#all'
+
+  match "home/data", :to => "home#data", :as => "data", :via => "get"
+  match "home/db_action", :to => "home#db_action", :as => "db_action", :via => "get"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
